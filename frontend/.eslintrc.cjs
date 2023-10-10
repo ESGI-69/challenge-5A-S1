89a3566b0539595f73c6c0b86f9dbe4c6ffd5f30
mrpinkcat <gatien.anizan@gmail.com>
2023-10-10 17:04:36 +0200

    init: vite react front with linter & CI

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,65 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    quotes: [ "error", "single" ],
+    "comma-dangle": [ "error", "always-multiline" ],
+    "no-console": [ "warn", { "allow": [ "warn", "error" ] } ],
+    "quote-props": [ "error", "as-needed" ],
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 }
+    ],
+    "object-curly-spacing": [ "error", "always" ],
+    "object-shorthand": [ "error", "always" ],
+    "array-bracket-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minItems": 3
+      }
+    ],
+    "array-bracket-spacing": [ "error", "always" ],
+    "array-element-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minItems": 3
+      }
+    ],
+    "arrow-body-style": [ "error", "as-needed" ],
+    "semi": [ "error", "always" ],
+    "no-else-return": "error",
+    "no-trailing-spaces": "error",
+    "prefer-destructuring": [
+      "error",
+      {
+        "array": true,
+        "object": true
+      },
+      {
+        "enforceForRenamedProperties": false
+      }
+    ],
+    "eol-last": [ "error", "always" ],
+    "keyword-spacing": [ "error", { "before": true, "after": true } ],
+    "comma-spacing": [ "error", { "before": false, "after": true } ],
+    "no-empty": [ "error", { "allowEmptyCatch": true } ],
+    "prefer-template": "error"
+  },
+}
