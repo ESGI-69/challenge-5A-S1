c985e50c37cfc109c0951c444e05f9f732e851a3
timdev0 <quentin.peltier.pro@gmail.com>
2023-10-16 17:09:06 +0200

    feat(Input): add style

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -7,11 +7,19 @@ export default function Input({
 
 }) {
 
+  // better way to handle css and var ?
+  // https://vitejs.dev/guide/features.html#css-modules or https://styled-components.com/ ??
+  const customStyle = {
+    borderRadius: '4px',
+    border: '1px solid var(--Text-regular, #272727)',
+    padding: '4px 8px',
+    fontSize: '14px',
+  };
 
   return (
     <>
       <input
-        // style={customStyle}
+        style={customStyle}
         id={id}
         type={type}
         placeholder={placeholder}
