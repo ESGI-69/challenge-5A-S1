c4109166f8ace4cc715f049fa0368130d9fd39ea
ycenta <thorbenliya@gmail.com>
2023-10-24 01:33:13 +0200

    feat(button_component): use forward ref react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Tabs, Tab, TabContent, TabsList } from '@/components/lib/Tabs';
 import { useTranslation } from 'react-i18next';
 import LanguageSwticher from './components/LanguageSwitcher';
 import Input from './components/lib/Input';
-import Button from './components/Button';
+import Button from './components/lib/Button/Index';
 
 function App() {
   const { t } = useTranslation('main');
@@ -34,7 +34,7 @@ function App() {
         </button>
         <Input id='Test' placeholder="Caca" onChange={handleInputChange} />
         <p>Input value : {inputValue} </p>
-        <Button variant="danger" onClick={() => alert('Danger!')}>
+        <Button variant="primary" onClick={() => alert('Danger!')} href="">
           {t('buttontest')}
         </Button>
         <p>
