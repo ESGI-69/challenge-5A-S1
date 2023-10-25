ea211fec1df4eb580821f3b0c1a43a035099e297
Samuel GUENIER <guenier.samuel@gmail.com>
2023-10-26 01:06:22 +0200

    fix(migration): duplicate sql in some migrations

--- backend/migrations/Version20231023151933.php
+++ backend/migrations/Version20231023151933.php
@@ -20,33 +20,20 @@ final class Version20231023151933 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE company_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE employee_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE establishment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE company (id INT NOT NULL, name VARCHAR(255) NOT NULL, path_kbis VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, validated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, rejected_reason TEXT DEFAULT NULL, logo_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN company.validated_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE employee (id INT NOT NULL, company_id_id INT NOT NULL, prefered_establishment_id INT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, avatar VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_5D9F75A138B53C32 ON employee (company_id_id)');
         $this->addSql('CREATE INDEX IDX_5D9F75A152BAE862 ON employee (prefered_establishment_id)');
-        $this->addSql('CREATE TABLE establishment (id INT NOT NULL, company_id INT NOT NULL, name VARCHAR(55) NOT NULL, email VARCHAR(55) NOT NULL, city VARCHAR(255) NOT NULL, street VARCHAR(255) NOT NULL, zip_code VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, lat NUMERIC(10, 6) NOT NULL, long NUMERIC(10, 6) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_DBEFB1EE979B1AD6 ON establishment (company_id)');
         $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A138B53C32 FOREIGN KEY (company_id_id) REFERENCES company (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A152BAE862 FOREIGN KEY (prefered_establishment_id) REFERENCES establishment (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE establishment ADD CONSTRAINT FK_DBEFB1EE979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE company_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE employee_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE establishment_id_seq CASCADE');
         $this->addSql('ALTER TABLE employee DROP CONSTRAINT FK_5D9F75A138B53C32');
         $this->addSql('ALTER TABLE employee DROP CONSTRAINT FK_5D9F75A152BAE862');
-        $this->addSql('ALTER TABLE establishment DROP CONSTRAINT FK_DBEFB1EE979B1AD6');
-        $this->addSql('DROP TABLE company');
         $this->addSql('DROP TABLE employee');
-        $this->addSql('DROP TABLE establishment');
     }
 }
