cc857788c50ffea3f8d74386f34b5746a6476086
mrpinkcat <gatien.anizan@gmail.com>
2023-10-25 11:43:27 +0200

    fix: linter

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { fileURLToPath, URL } from 'node:url';
+import { URL, fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
