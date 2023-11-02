f8c0809e67649ecbe6b073ae1458d2ffe706e386
timdev0 <quentin.peltier.pro@gmail.com>
2023-11-02 17:54:36 +0100

    feat(ServicesTable): remove show more btn

--- frontend/src/components/Services/ServicesRow/index.jsx
+++ frontend/src/components/Services/ServicesRow/index.jsx
@@ -2,8 +2,6 @@ import style from './ServicesRow.module.scss';
 import PropTypes from 'prop-types';
 import Button from '../../lib/Button';
 import { minToDuration } from '@/utils/formater/time';
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
 function ServicesRow({
   name,
@@ -11,27 +9,15 @@ function ServicesRow({
   price,
   duration,
 }) {
-  const { t } = useTranslation('servicesTable');
-
-  const [ isExpanded, setIsExpanded ] = useState(false);
-
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
-
   return (
     <div className={style.ServicesRow}>
       <div className={style.ServicesRowDesc}>
         <span>{name}</span>
-        <div className={`${style.ServicesRowDescDetail} ${isExpanded ? style.Expanded : style.Collapsed}`}>
+        <div className={style.ServicesRowDescDetail}>
           <span>
             {description}
           </span>
         </div>
-        <button
-          className={style.ServicesRowDescShowMore}
-          onClick={toggleExpand}>{isExpanded ? t('less') : t('more')}
-        </button>
       </div>
       <div className={style.ServicesRowInfos}>
         <span>{minToDuration(duration)}</span>
