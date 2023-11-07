674a9fb537ca2a7627725647cfabdc1d74af7cfe
mrpinkcat <gatien.anizan@gmail.com>
2023-11-07 11:48:27 +0100

    fix(i18n): Remove unnecessary await statement in i18n.js.

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-await i18next
+i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(Backend)
