ebc0f91bfabd7f207c5c507ff64123e19a1a0f03
mrpinkcat <gatien.anizan@gmail.com>
2023-11-08 08:54:32 +0100

    feat(Home): style websitelayout

--- frontend/src/pages/__layouts/websiteLayout/index.jsx
+++ frontend/src/pages/__layouts/websiteLayout/index.jsx
@@ -2,18 +2,17 @@ import PropTypes from 'prop-types';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
+import styles from './WebsiteLayout.module.scss';
 
 export default function WebsiteLayout() {
   return (
-    <>
-      <Header />
-      <div>
-        <Suspense fallback="Loading...">
-          <Outlet />
-        </Suspense>
-      </div>
-      <footer>Footer</footer>
-    </>
+    <div className={ styles.WebsiteLayout }>
+      <Suspense fallback="Loading...">
+        <Header />
+        <Outlet />
+        <footer>Footer</footer>
+      </Suspense>
+    </div>
   );
 }
 
--- frontend/src/pages/website/Home/index.jsx
+++ frontend/src/pages/website/Home/index.jsx
@@ -1,9 +1,14 @@
 import { useTranslation } from 'react-i18next';
+import backgroundImage from '@/assets/home-background.jpg';
+import styles from './Home.module.scss';
 
 export default function Home() {
   const { t } = useTranslation('main');
   return (
-    <main>
+    <main
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+      className={styles.Home}
+    >
       <h1>Home</h1>
       <p className="read-the-docs">
         {t('doc')}
