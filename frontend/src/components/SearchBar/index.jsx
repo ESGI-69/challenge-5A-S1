1e4c212e4ee534d544734d3ab7d1cee8b372987c
mrpinkcat <gatien.anizan@gmail.com>
2023-11-09 09:52:42 +0100

    feat(SearchBar): Add form submission functionality

--- frontend/src/components/SearchBar/index.jsx
+++ frontend/src/components/SearchBar/index.jsx
@@ -3,26 +3,49 @@ import { useTranslation } from 'react-i18next';
 import Input from '@/components/lib/Input';
 import Button from '@/components/lib/Button';
 import style from './SearchBar.module.scss';
+import { useState } from 'react';
 
 function SearchBar({
   className,
+  onSearch,
+  isLoading = false,
 }) {
   const { t } = useTranslation('searchbar');
+  const [ searchForm, setSearchForm ] = useState({ name: '', location: '' });
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    onSearch(searchForm);
+  };
 
   return (
-    <div className={`${style.SearchBar} ${className}`}>
-      <Input variant="no-border" placeholder={t('name.placeholder')} />
+    <form className={`${style.SearchBar} ${className}`} onSubmit={handleFormSubmission}>
+      <Input
+        onEnterPressed={onSearch}
+        variant="no-border"
+        placeholder={t('name.placeholder')}
+        disabled={isLoading}
+        onInput={(e) => setSearchForm((old) => ({ ...old, name: e.target.value }))}
+      />
       <div className={ style.SearchBarSeparator } />
-      <Input variant="no-border" placeholder={t('location.placeholder')} />
-      <Button variant="black">
+      <Input
+        onEnterPressed={onSearch}
+        variant="no-border"
+        placeholder={t('location.placeholder')}
+        disabled={isLoading}
+        onInput={(e) => setSearchForm((old) => ({ ...old, location: e.target.value }))}
+      />
+      <Button variant="black" type="submit">
         {t('search')}
       </Button>
-    </div>
+    </form>
   );
 }
 
 SearchBar.propTypes = {
   className: PropTypes.node,
+  onSearch: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool,
 };
 
 export default SearchBar;
