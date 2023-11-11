2e874a40049bc6be0c986d0e5dcdfa954e60bf76
ycenta <thorbenliya@gmail.com>
2023-11-12 00:16:31 +0100

    feat:popin +gallery, add popin to view images from gallery

---
+++ frontend/src/components/Popin/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Popin.module.scss';
+import Button from '../lib/Button';
+
+const Popin = React.forwardRef(function Popin(
+  { children, onClose, ...delegated },
+  ref,
+) {
+  return (
+    <div ref={ref} className={styles.Popin} {...delegated}>
+      <div className={styles.PopinContent}>
+        <div className={styles.PopinClose}>
+          <Button variant="primary" onClick={onClose}>X</Button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+});
+
+Popin.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Popin;
--- frontend/src/components/lib/Gallery/index.jsx
+++ frontend/src/components/lib/Gallery/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Gallery.module.scss';
 import Button from '../Button';
 import { useTranslation } from 'react-i18next';
+import Popin from '../../Popin';
 
 const Gallery = React.forwardRef(function Gallery(
   { children, ...delegated },
@@ -18,6 +19,28 @@ const Gallery = React.forwardRef(function Gallery(
   ];
   const { t } = useTranslation('gallery');
 
+  const [ galleryIsOpen, setGalleryIsOpen ] = React.useState(false);
+  const [ secondGalleryIsOpen, setSecondGalleryIsOpen ] = React.useState(false);
+
+  const [ pictureIndex, setPictureIndex ] = React.useState(0);
+
+  const openGallery = () => {
+    setGalleryIsOpen(true);
+  };
+
+  const closeGallery = () => {
+    setGalleryIsOpen(false);
+  };
+
+  const openSecondGallery = (index) => {
+    setSecondGalleryIsOpen(true);
+    setPictureIndex(index);
+  };
+
+  const closeSecondGallery = () => {
+    setSecondGalleryIsOpen(false);
+  };
+
   return (
     <div ref={ref} className={styles.Gallery} {...delegated}>
 
@@ -25,9 +48,33 @@ const Gallery = React.forwardRef(function Gallery(
         index > 4 ? null :
           <div key={index} className={styles[gridClassArray[index]]}>
             {picture}
-            {index === 4 && pictures.length > 5 ? <Button href="javascript:void" className={styles.GalleryMoreBtn}>{t('viewPhotos', { count: pictures.length })}</Button> : null}
+            {index === 4 && pictures.length > 5 ? <Button href="javascript:void" onClick={openGallery} className={styles.GalleryMoreBtn}>{t('viewPhotos', { count: pictures.length })}</Button> : null}
           </div>
       ))}
+
+      {galleryIsOpen && (
+        <Popin onClose={closeGallery}>
+          <div className={styles.GalleryPopin}>
+            {pictures.map((picture, index) => (
+              <div key={index} className={styles.GalleryPopinImg} onClick={() => openSecondGallery(index)}>
+                {picture}
+              </div>
+            ))}
+          </div>
+        </Popin>
+      )}
+
+      {secondGalleryIsOpen && (
+        <Popin onClose={closeSecondGallery}>
+          <div className={styles.GallerySlideshow}>
+            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex - 1 + pictures.length) % pictures.length)}>←</Button>
+            <div className={styles.GallerySlideshow}>
+              {pictures[pictureIndex]}
+            </div>
+            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex + 1) % pictures.length)}>→</Button>
+          </div>
+        </Popin>
+      )}
 
     </div>
   );
