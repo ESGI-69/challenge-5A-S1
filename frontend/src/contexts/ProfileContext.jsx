6316c9ef0060a34e888e42381d7f61f1a808d8cd
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-11-12 04:26:36 +0100

    lint: fixed lint errors

--- frontend/src/contexts/ProfileContext.jsx
+++ frontend/src/contexts/ProfileContext.jsx
@@ -43,6 +43,7 @@ export default function ProfileProvider({ children }) {
 
   useEffect(() => {
     verifyToken(Cookies.get('token'));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   /**
