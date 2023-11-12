36e6506dd4337c5f7455a81a70b9300733279a71
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-11-12 02:45:32 +0100

    refactor: restyled login & register pages

--- frontend/src/pages/website/Login/index.jsx
+++ frontend/src/pages/website/Login/index.jsx
@@ -3,18 +3,24 @@ import Button from '@/components/lib/Button';
 import styles from './Login.module.scss';
 import LoginForm from './LoginForm';
 import { useTranslation } from 'react-i18next';
+import sideImage from '@/assets/leaves.jpg';
 
 export default function Login() {
   const { t } = useTranslation('login');
   return (
-    <div className={styles.Login}>
-      <h1 className={styles.LoginTitle}>{t('title')}</h1>
-      <LoginForm />
-      <hr className={styles.LoginSeparator} />
-      <div className={styles.LoginRegister}>
-        <p className={styles.LoginRegisterText}>{t('registerTitle')}</p>
-        <Button href="/register">{t('createAccount')}</Button>
+    <div className={styles.Page}>
+      <div className={styles.Login}>
+        <h1 className={styles.LoginTitle}>{t('title')}</h1>
+        <LoginForm />
+        <hr className={styles.LoginSeparator} />
+        <div className={styles.LoginRegister}>
+          <p className={styles.LoginRegisterText}>{t('registerTitle')}</p>
+          <Button to="/register" variant="black" isPlain>{t('createAccount')}</Button>
+        </div>
       </div>
+      <div className={styles.PageImage} style={{
+        backgroundImage: `url(${sideImage})`,
+      }}></div>
     </div>
   );
 }
