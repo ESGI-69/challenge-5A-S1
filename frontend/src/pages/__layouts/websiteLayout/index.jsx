e92a82a8379cb41fbd70f45cae72f5ac1a3a4931
mrpinkcat <gatien.anizan@gmail.com>
2023-11-13 01:12:48 +0100

    feat(FullPage): add full page style path

--- frontend/src/pages/__layouts/websiteLayout/index.jsx
+++ frontend/src/pages/__layouts/websiteLayout/index.jsx
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/Header';
 import styles from './WebsiteLayout.module.scss';
 
 export default function WebsiteLayout() {
+  const { pathname } = useLocation();
+
+  const fullPagePaths = [
+    '/',
+    '/login',
+    '/register',
+  ];
+
+  const isFullPage = fullPagePaths.includes(pathname) ? styles.WebsiteLayoutFullPage : '';
+
   return (
-    <div className={ styles.WebsiteLayout }>
+    <div className={ `${styles.WebsiteLayout} ${isFullPage}` }>
       <Suspense fallback="Loading...">
         <Header />
         <div className={styles.WebsiteLayoutContent}>
