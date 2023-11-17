7c257631d17bc386cda04bfae45f5a13c357e382
ycenta <thorbenliya@gmail.com>
2023-11-17 18:24:02 +0100

    fix bugged button in gallery, add arrow icon

--- frontend/src/components/Gallery/index.jsx
+++ frontend/src/components/Gallery/index.jsx
@@ -4,6 +4,7 @@ import styles from './Gallery.module.scss';
 import Button from '@/components/lib/Button';
 import { useTranslation } from 'react-i18next';
 import Popin from '@/components/Popin';
+import Arrow from '@/components/lib/Icons/Arrow';
 
 const Gallery = React.forwardRef(function Gallery(
   { children, ...delegated },
@@ -48,7 +49,7 @@ const Gallery = React.forwardRef(function Gallery(
         index > 4 ? null :
           <div key={index} className={styles[gridClassArray[index]]}>
             {picture}
-            {index === 4 && pictures.length > 5 ? <Button variant="secondary"  onClick={openGallery} className={styles.GalleryMoreBtn}>{t('viewPhotos', { count: pictures.length })}</Button> : null}
+            {index === 4 && pictures.length > 5 ? <div className={styles.GalleryMoreBtn}> <Button variant="black" onClick={openGallery}>{t('viewPhotos', { count: pictures.length })}</Button></div> : null}
           </div>
       ))}
 
@@ -67,11 +68,11 @@ const Gallery = React.forwardRef(function Gallery(
       {secondGalleryIsOpen && (
         <Popin onClose={closeSecondGallery}>
           <div className={styles.GallerySlideshow}>
-            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex - 1 + pictures.length) % pictures.length)}>←</Button>
+            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex - 1 + pictures.length) % pictures.length)}><Arrow style={{ transform: 'rotate(-90deg)' }}></Arrow></Button>
             <div className={styles.GallerySlideshow}>
               {pictures[pictureIndex]}
             </div>
-            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex + 1) % pictures.length)}>→</Button>
+            <Button className={styles.GallerySlideshowArrow} onClick={() => setPictureIndex((pictureIndex + 1) % pictures.length)}><Arrow style={{ transform: 'rotate(90deg)' }}></Arrow></Button>
           </div>
         </Popin>
       )}
