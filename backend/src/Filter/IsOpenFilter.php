b3ecba692238d77719a2b87c0c2910b94e32c5ed
ycenta <thorbenliya@gmail.com>
2023-11-23 00:01:57 +0100

    Add custom filter isOpen for establishement using dql

--- backend/src/Filter/IsOpenFilter.php
+++ backend/src/Filter/IsOpenFilter.php
@@ -8,32 +8,38 @@ use ApiPlatform\Metadata\Operation;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\PropertyInfo\Type;
 
-
-//filter that check if the current time is between the opening hours of the establishment (start_time and end_time)
+//filter that checks if the current time is between the opening hours of the establishment (start_time and end_time)
 final class IsOpenFilter extends AbstractFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
-        if (
-            !$this->isPropertyEnabled($property, $resourceClass) ||
-            !$this->isPropertyMapped($property, $resourceClass)
-        ) {
+
+        if (!isset($context['filters']['is_open']) || $context['filters']['is_open'] == "false") {
             return;
         }
 
         $rootAlias = $queryBuilder->getRootAliases()[0];
-        $queryBuilder->leftJoin($rootAlias . '.openingHours', 'opening_hour');
-        $queryBuilder->andWhere('opening_hour.startTime <= :now')
-            ->andWhere('opening_hour.endTime >= :now')
-            ->setParameter('now', date('H:i:s'));
+        $parameterName = $queryNameGenerator->generateParameterName('isOpen');
+    
+        $subqueryAlias = $queryNameGenerator->generateJoinAlias('oh');
+        
+        //make a query that returns the establishment_id of the opening hours that are open at the current time
+        $subquery = $this->managerRegistry->getManagerForClass($resourceClass)->createQueryBuilder()
+            ->select('IDENTITY('.$subqueryAlias.'.establishment)')
+            ->from('App\Entity\OpeningHour', $subqueryAlias)
+            ->where($subqueryAlias.'.startTime <= :currentTime')
+            ->andWhere($subqueryAlias.'.endTime >= :currentTime')
+            ->getDQL();
 
-      
+        //add the subquery to the main query
+        $queryBuilder
+            ->andWhere($rootAlias.'.id IN ('.$subquery.')')
+            ->setParameter('currentTime', date('H:i:s'));
 
     }
 
     public function getDescription(string $resourceClass): array
     {
-       
         $description['is_open'] = [
             'property' => null,
             'type' => Type::BUILTIN_TYPE_BOOL,
