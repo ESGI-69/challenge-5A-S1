41b433fab43c41ee7fac5705b1400ea65f4fec57
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2023-12-06 15:23:44 +0100

    feat: added fixtures for Company

--- backend/src/Story/DefaultUsersStory.php
+++ backend/src/Story/DefaultUsersStory.php
@@ -2,8 +2,10 @@
 
 namespace App\Story;
 
+use App\Factory\CompanyFactory;
 use App\Factory\UserFactory;
 use Zenstruck\Foundry\Story;
+use function Zenstruck\Foundry\lazy;
 
 final class DefaultUsersStory extends Story
 {
@@ -11,5 +13,24 @@ final class DefaultUsersStory extends Story
     {
         // TODO build your story here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#stories)
         UserFactory::createMany(20);
+        UserFactory::createMany(10, [
+          'company' => lazy(fn() => CompanyFactory::createOne()),
+        ]);
+        UserFactory::createOne([
+          'email' => 'admin@platiny.com',
+          'firstname' => 'Chad',
+          'lastname' => 'Giga',
+          'password' => password_hash('password', PASSWORD_BCRYPT),
+          'phonenumber' => '+3369696969',
+          'roles' => ['ROLE_USER', 'ROLE_ADMIN'],
+        ]);
+        UserFactory::createOne([
+          'email' => 'user@platiny.com',
+          'firstname' => 'Peon',
+          'lastname' => 'Juste',
+          'password' => password_hash('password', PASSWORD_BCRYPT),
+          'phonenumber' => '+3369696969',
+          'roles' => ['ROLE_USER'],
+        ]);
     }
 }
