205a60e569be5ede79b16dfeb010dfb02d1da4e2
ycenta <thorbenliya@gmail.com>
2023-12-14 00:49:20 +0100

    feat(reservation): add schedule component

--- frontend/src/components/Schedule/index.jsx
+++ frontend/src/components/Schedule/index.jsx
@@ -3,14 +3,47 @@ import styles from './Schedule.module.scss';
 
 const Schedule = React.forwardRef(function Schedule(
   {
+    schedule,
     children,
     ...delegated
   },
   ref,
 ){
 
+  const fakeDivs = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+  ];
+
   return (
-    <h2>Schedule</h2>
+    <>
+      <div className={styles.Schedule}>
+        <div className={styles.ColumnsContainer}>
+
+          {fakeDivs.map((div, index) => (
+            <div className={styles.Column} key={index}>
+              <div className={styles.ColumnHeader}>
+                <span className={styles.ColumnTitle}>Lundi</span>
+                <span className={styles.ColumnDate}>14 dec.</span>
+              </div>
+              <div className={styles.ColumnBody}>
+                <button className={styles.ColumnButton}>10:00</button>
+                <button className={styles.ColumnButton}>10:30</button>
+                <button className={styles.ColumnButton}>11:00</button>
+                <button className={styles.ColumnButton}>11:30</button>
+              </div>
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+    </>
   );
 });
 
