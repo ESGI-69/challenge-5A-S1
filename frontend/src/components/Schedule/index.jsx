579174562c4fdc2ff044765a4b1bcdcf134c30f8
ycenta <thorbenliya@gmail.com>
2023-12-14 14:14:51 +0100

    feat(schedule-page): add scroll animation on schedule container and emptyArrow icon

--- frontend/src/components/Schedule/index.jsx
+++ frontend/src/components/Schedule/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './Schedule.module.scss';
+import EmptyArrow from '../lib/Icons/EmptyArrow';
 
 const Schedule = React.forwardRef(function Schedule(
   {
@@ -9,6 +11,8 @@ const Schedule = React.forwardRef(function Schedule(
   },
   ref,
 ){
+
+  const [ currentWeek, setCurrentWeek ] = useState(0);
 
   const fakeDivs = [
     1,
@@ -20,26 +24,62 @@ const Schedule = React.forwardRef(function Schedule(
     7,
   ];
 
+  const handleArrowClick = (direction) => {
+    if (direction === 'prev') {
+      setCurrentWeek((prevWeek) => Math.max(prevWeek - 1, 0));
+    } else if (direction === 'next') {
+      setCurrentWeek((prevWeek) => Math.min(prevWeek + 1, fakeDivs.length - 1));
+    }
+  };
+
   return (
     <>
       <div className={styles.Schedule}>
-        <div className={styles.ColumnsContainer}>
+        <div className={styles.ScheduleArrows}>
+          <span className={styles.ScheduleArrowsLeft}  onClick={() => handleArrowClick('prev')}><EmptyArrow style={{ width:40 }} /></span>
+          <span className={styles.ScheduleArrowsRight}  onClick={() => handleArrowClick('next')}><EmptyArrow style={{ width:40 }} /></span>
+        </div>
+        <div className={styles.ScrollContainer}>
+          <div className={styles.FlexContainer} style={{ transform: `translateX(${currentWeek * -100}%)` }}>
+            <div className={styles.ColumnsContainer}>
 
-          {fakeDivs.map((div, index) => (
-            <div className={styles.Column} key={index}>
-              <div className={styles.ColumnHeader}>
-                <span className={styles.ColumnTitle}>Lundi</span>
-                <span className={styles.ColumnDate}>14 dec.</span>
-              </div>
-              <div className={styles.ColumnBody}>
-                <button className={styles.ColumnButton}>10:00</button>
-                <button className={styles.ColumnButton}>10:30</button>
-                <button className={styles.ColumnButton}>11:00</button>
-                <button className={styles.ColumnButton}>11:30</button>
-              </div>
+              {fakeDivs.map((div, index) => (
+                <div className={styles.Column} key={index}>
+                  <div className={styles.ColumnHeader}>
+                    <span className={styles.ColumnTitle}>Lundi</span>
+                    <span className={styles.ColumnDate}>14 dec.</span>
+                  </div>
+                  <div className={styles.ColumnBody}>
+                    <button className={styles.ColumnButton}>10:00</button>
+                    <button className={styles.ColumnButton}>10:30</button>
+                    <button className={styles.ColumnButton}>11:00</button>
+                    <button className={styles.ColumnButton}>11:30</button>
+                  </div>
+                </div>
+              ))}
+
             </div>
-          ))}
 
+            <div className={styles.ColumnsContainer}>
+
+              {fakeDivs.map((div, index) => (
+                <div className={styles.Column} key={index}>
+                  <div className={styles.ColumnHeader}>
+                    <span className={styles.ColumnTitle}>Lundi</span>
+                    <span className={styles.ColumnDate}>14 dec.</span>
+                  </div>
+                  <div className={styles.ColumnBody}>
+                    <button className={styles.ColumnButton}>10:00</button>
+                    <button className={styles.ColumnButton}>10:30</button>
+                    <button className={styles.ColumnButton}>11:00</button>
+                    <button className={styles.ColumnButton}>11:30</button>
+                  </div>
+                </div>
+              ))}
+
+            </div>
+
+          </div>
         </div>
 
       </div>
