4ccef6cbae92148528b104aad5b5205dd264c83f
ycenta <thorbenliya@gmail.com>
2023-12-14 14:14:51 +0100

    feat(schedule-page): add scroll animation on schedule container and emptyArrow icon

---
+++ frontend/src/components/lib/Icons/EmptyArrow.jsx
@@ -0,0 +1,7 @@
+export default function EmptyArrow({ ...delegated }) {
+  return (
+    <svg style={{ width: '16px' }}viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...delegated}>
+      <path d="M11.15 8.75L14.65 12L11.15 15.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+    </svg>
+  );
+}
