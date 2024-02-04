ea8eb64a209b4230ed5af0618f64a055c521066c
mrpinkcat <gatien.anizan@gmail.com>
2024-02-04 19:35:13 +0100

    feat(EstablishmentUpdate): add update form

--- frontend/src/pages/backoffice/Establishments/EstablishmentUpdate/index.jsx
+++ frontend/src/pages/backoffice/Establishments/EstablishmentUpdate/index.jsx
@@ -1,8 +1,9 @@
 import BackofficeHeader from '@/components/BackofficeHeader';
-// import EstablishmentUpdateForm from '@/components/EstablishmentUpdateForm';
+import EstablishmentUpdateForm from '@/components/EstablishmentUpdateForm';
 import Button from '@/components/lib/Button';
 import OpeningHoursSelector from '@/components/OpeningHoursSelector';
 import { EstablishmentContext } from '@/contexts/api/EstablishmentContext';
+import { EstablishmentTypeContext } from '@/contexts/api/EstablishmentTypeContext';
 import { useContext, useEffect } from 'react';
 
 import { useTranslation } from 'react-i18next';
@@ -10,10 +11,16 @@ import { useParams } from 'react-router-dom';
 
 export default function EstablishmentUpdate() {
   const { establishment, getById, isEstablishmentLoading } = useContext(EstablishmentContext);
+  const { establishmentTypes, get: getEstablishmentTypes, isEstablishmentTypesLoading } = useContext(EstablishmentTypeContext);
   const { id } = useParams();
   useEffect(() => {
     getById(id);
+    getEstablishmentTypes();
   }, []);
+
+  const updateEstablishment = (data) => {
+    console.log(data);
+  };
 
   const { t } = useTranslation('establishment');
   return (
@@ -22,7 +29,13 @@ export default function EstablishmentUpdate() {
         <h1>{ t('update.title') }</h1>
       </BackofficeHeader>
       <Button to="/backoffice/establishments">{ t('update.back') }</Button>
-      {/* <EstablishmentUpdateForm /> */}
+      {(!isEstablishmentTypesLoading && !isEstablishmentLoading && establishment && establishmentTypes) && (
+        <EstablishmentUpdateForm
+          establishmentTypes={establishmentTypes}
+          onSubmit={updateEstablishment}
+          {...establishment}
+        />
+      )}
       <h2>{ t('openingHourSelector') }</h2>
       {(!isEstablishmentLoading && establishment) && <OpeningHoursSelector />}
     </>
