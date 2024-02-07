8440770ba1c92a6c69b98971b03da3c62d8bc2c2
Samuel GUENIER <guenier.samuel@gmail.com>
2024-02-07 23:04:07 +0100

    feat(BackOffice employee): add translation

--- frontend/src/pages/backoffice/Employees/index.jsx
+++ frontend/src/pages/backoffice/Employees/index.jsx
@@ -2,19 +2,22 @@ import Button from '@/components/lib/Button';
 import BackofficeHeader from '@/components/BackofficeHeader';
 import EmployeesTable from './EmployeesTable';
 import EmployeeProvider from '@/contexts/api/EmployeeContext';
+import { useTranslation } from 'react-i18next';
 
 export default function Employees() {
+
+  const { t } = useTranslation('employee');
 
   return (
     <>
       <BackofficeHeader
         actionsComponent={
           <Button to="/backoffice/employees/create">
-            Créer un employé
+            {t('list.actions.create')}
           </Button>
         }
       >
-        <h1>Employés</h1>
+        <h1>{t('list.title')}</h1>
       </BackofficeHeader>
       <EmployeeProvider>
         <EmployeesTable></EmployeesTable>
