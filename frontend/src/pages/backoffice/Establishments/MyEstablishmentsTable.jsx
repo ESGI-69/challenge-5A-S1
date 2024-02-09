d34340eac6cc8cecb2070130d647176bee76255b
mrpinkcat <gatien.anizan@gmail.com>
2024-02-09 20:54:08 +0100

    feat(Establishment): removing establishment

--- frontend/src/pages/backoffice/Establishments/MyEstablishmentsTable.jsx
+++ frontend/src/pages/backoffice/Establishments/MyEstablishmentsTable.jsx
@@ -2,8 +2,12 @@ import { useTranslation } from 'react-i18next';
 import PTable from '@/components/lib/PTable';
 import { EstablishmentContext } from '@/contexts/api/EstablishmentContext';
 import { ProfileContext } from '@/contexts/ProfileContext';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
+import style from './MyEstablishmentsTable.module.scss';
+import { XMarkIcon } from '@heroicons/react/20/solid';
+import Button from '@/components/lib/Button';
 
 export default function MyEstablishmentsTable() {
   const { t } = useTranslation('establishment');
@@ -12,12 +16,23 @@ export default function MyEstablishmentsTable() {
     get,
     establishments,
     isEstablishmentsLoading,
+    deleteEstablishment,
+    isDeleteEstablishmentLoading,
   } = useContext(EstablishmentContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     get({ 'company.id': profile.company.id });
   }, []);
+
+  const [ isConfirmDeletionModalOpen, setIsConfirmDeletionModalOpen ] = useState(false);
+  const [ establishmentToDelete, setEstablishmentToDelete ] = useState(null);
+
+  const openDeletionConfirmationModal = (establishment) => {
+    console.log(establishment);
+    setEstablishmentToDelete(establishment);
+    setIsConfirmDeletionModalOpen(true);
+  };
 
   const DATA_TEMPLATE = {
     properties: {
@@ -53,18 +68,73 @@ export default function MyEstablishmentsTable() {
   };
 
   return (
-    <>
+    <div>
       <PTable
         template={DATA_TEMPLATE}
         data={establishments}
         loading={isEstablishmentsLoading}
         actions={[
           {
-            name: 'view',
+            name: t('table.actions.edit'),
             onClick: ({ id }) => navigate(`/backoffice/establishments/${id}`),
+          },
+          {
+            name: t('table.actions.delete'),
+            onClick: (establishment) => openDeletionConfirmationModal(establishment),
+            // onClick: async ({ id }) => {
+            //   await deleteEstablishment(id),
+            //   get({ 'company.id': profile.company.id });
+            // },
           },
         ]}
       />
-    </>
+      <Modal
+        isOpen={isConfirmDeletionModalOpen}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.75)',
+          },
+          content: {
+            width: '500px',
+            height: 'fit-content',
+            margin: 'auto',
+          },
+        }}
+
+      >
+        <div
+          className={style.MyEstablishmentsTableModal}
+        >
+          <XMarkIcon
+            className={style.MyEstablishmentsTableModalClose}
+            onClick={() => setIsConfirmDeletionModalOpen(false) && setEstablishmentToDelete(null)}
+          />
+          <h2>{t('modal.deletion.title')}</h2>
+          <p>{t('modal.deletion.content', { city: establishmentToDelete?.city })}</p>
+          <div
+            className={style.MyEstablishmentsTableModalButtons}
+          >
+            <Button
+              variant="danger"
+              disabled={isDeleteEstablishmentLoading}
+              onClick={async () => {
+                await deleteEstablishment(establishmentToDelete.id);
+                get({ 'company.id': profile.company.id });
+                setIsConfirmDeletionModalOpen(false);
+                setEstablishmentToDelete(null);
+              }}>
+              {t('confirm', { ns: 'base' })}
+            </Button>
+            <Button
+              disabled={isDeleteEstablishmentLoading}
+              onClick={() => setIsConfirmDeletionModalOpen(false) && setEstablishmentToDelete(null)}
+            >
+              {t('cancel', { ns: 'base' })}
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 }
