c44425c3124fb691547389914dab77ee8732ac35
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2024-02-10 09:18:13 +0100

    feat: added relative date data to stats

--- backend/src/Controller/Statistics/CompanyStatisticsController.php
+++ backend/src/Controller/Statistics/CompanyStatisticsController.php
@@ -3,16 +3,10 @@
 namespace App\Controller\Statistics;
 
 use App\Entity\Company;
-use App\Entity\User;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 
 #[AsController]
@@ -21,23 +15,98 @@ class CompanyStatisticsController
   public function __construct(
     protected Security $security,
     private readonly NormalizerInterface $normalizer,
-  ) {}
+  ) {
+  }
 
   public function __invoke(Company $company): JsonResponse
-    {
-        $establishments = $company->getEstablishments();
-        $employees = $company->getEmployees();
-        $appointments = [];
-        foreach ($establishments as $establishment) {
-            array_push($appointments, $establishment->getAppointments());
+  {
+    $thisWeekMonday = date('Y-m-d', strtotime('monday this week'));
+    $thisWeekSunday = date('Y-m-d', strtotime('sunday this week'));
+    $firstDayOfTheMonth = date('Y-m-01');
+    $lastDayOfTheMonth = date('Y-m-t');
+    
+    $establishments = $company->getEstablishments();
+    $employees = $company->getEmployees();
+
+    $companyAppointments = [];
+    $companyAppointmentsSum = 0;
+
+    $companyAppointmentsWeeklySum = 0;
+    $establishmentsWeeklySums = [];
+    $employeesWeeklyPerformance = [];
+
+    $companyAppointmentsMonthlySum = 0;
+    $establishmentsMonthlySums = [];
+    $employeesMonthlyPerformance = [];
+
+    $sumPerDayAllTime = [];
+
+    foreach ($establishments as $establishment) {
+      $establishmentAppointments = $establishment->getAppointments();
+      array_push($companyAppointments, $establishmentAppointments);
+      foreach ($establishmentAppointments as $appointment) {
+        $appointmentDate = $appointment->getStartDate()->format('Y-m-d');
+        // Weekly process
+        if ($appointmentDate >= $thisWeekMonday && $appointmentDate <= $thisWeekSunday) {
+          // Total sum for all establishments
+          $companyAppointmentsWeeklySum += $appointment->getPrice();
+          // Total sum for each establishment
+          if (array_key_exists($establishment->getId(), $establishmentsWeeklySums)) {
+            $establishmentsWeeklySums[$establishment->getId()] += $appointment->getPrice();
+          } else {
+            $establishmentsWeeklySums[$establishment->getId()] = $appointment->getPrice();
+          }
+          // Total sum for each employees
+          if (array_key_exists($appointment->getEmployee()->getId(), $employeesWeeklyPerformance)) {
+            $employeesWeeklyPerformance[$appointment->getEmployee()->getId()] += $appointment->getPrice();
+          } else {
+            $employeesWeeklyPerformance[$appointment->getEmployee()->getId()] = $appointment->getPrice();
+          }
         }
-
-
-        return new JsonResponse([
-            'establishments' => $this->normalizer->normalize($establishments),
-            'employees' => $this->normalizer->normalize($employees),
-            'appointments' => $this->normalizer->normalize($appointments),
-        ]);
+        // Monthly process
+        if ($appointmentDate >= $firstDayOfTheMonth && $appointmentDate <= $lastDayOfTheMonth) {
+          // Total sum for all establishments
+          $companyAppointmentsMonthlySum += $appointment->getPrice();
+          // Total sum for each establishment
+          if (array_key_exists($establishment->getId(), $establishmentsMonthlySums)) {
+            $establishmentsMonthlySums[$establishment->getId()] += $appointment->getPrice();
+          } else {
+            $establishmentsMonthlySums[$establishment->getId()] = $appointment->getPrice();
+          }
+          // Total sum for each employees
+          if (array_key_exists($appointment->getEmployee()->getId(), $employeesMonthlyPerformance)) {
+            $employeesMonthlyPerformance[$appointment->getEmployee()->getId()] += $appointment->getPrice();
+          } else {
+            $employeesMonthlyPerformance[$appointment->getEmployee()->getId()] = $appointment->getPrice();
+          }
+        }
+        // All time process
+        $companyAppointmentsSum += $appointment->getPrice();
+        if (array_key_exists($appointmentDate, $sumPerDayAllTime)) {
+          $sumPerDayAllTime[$appointmentDate] += $appointment->getPrice();
+        } else {
+          $sumPerDayAllTime[$appointmentDate] = $appointment->getPrice();
+        }
+      }
     }
+
+    
+
+
+
+    return new JsonResponse([
+      'establishments' => $this->normalizer->normalize($establishments),
+      'employees' => $this->normalizer->normalize($employees),
+      'appointments' => $this->normalizer->normalize($companyAppointments),
+      'totalNumberOfAppointments' => count($companyAppointments),
+      'companyAllTimeSum' => $companyAppointmentsSum,
+      'companyWeeklySum' => $companyAppointmentsWeeklySum,
+      'establishmentsWeeklySums' => $establishmentsWeeklySums,
+      'employeesWeeklyPerformance' => $employeesWeeklyPerformance,
+      'companyMonthlySum' => $companyAppointmentsMonthlySum,
+      'establishmentsMonthlySums' => $establishmentsMonthlySums,
+      'employeesMonthlyPerformance' => $employeesMonthlyPerformance,
+      'sumPerDayAllTime' => $sumPerDayAllTime,
+    ]);
+  }
 }
- 
