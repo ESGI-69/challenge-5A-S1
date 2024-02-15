52c3ff6cae56b84e89f107166e409208c8d4f591
Samuel GUENIER <guenier.samuel@gmail.com>
2024-02-15 16:35:41 +0100

    add : php unit test + job ci to run tests + .env.example front

---
+++ backend/tests/TestOfTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+class TestOfTest extends ApiTestCase
+{
+
+    public function testOfTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
